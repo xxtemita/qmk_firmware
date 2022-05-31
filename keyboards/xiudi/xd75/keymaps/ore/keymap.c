7f2dc8e97f38c9d88bbcc9c6f9c44c17c30debbe
xxtemita <79151569+xxtemita@users.noreply.github.com>
2022-05-31 20:02:19 +0900

    最下段(カーソルキー)周り変更

--- keyboards/xiudi/xd75/keymaps/ore/keymap.c
+++ keyboards/xiudi/xd75/keymaps/ore/keymap.c
@@ -34,6 +34,8 @@ enum layer_names {
 
 // Special keycodes
 #define SPC_SFT SFT_T(KC_SPC)
+#define EQL_ALT RALT_T(KC_EQL)
+#define QUO_ALT LALT_T(KC_QUOT)
 #define MIN_CTL RCTL_T(KC_MINS)
 #define GRV_CTL LCTL_T(KC_GRV)
 #define MIN_GUI RGUI_T(KC_MINS)
@@ -63,9 +65,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_WBASE] = LAYOUT_ortho_5x15(
         ADJUST,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LCBR, KC_EQL,  KC_RCBR, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_BSLS, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LPRN, KC_QUOT, KC_RPRN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_HOME, KC_UP,   KC_END,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_F13,  KC_LGUI, KC_LALT, WFN,     GRV_CTL, SPC_SFT, KC_LEFT, KC_DOWN, KC_RGHT, SPC_SFT, MIN_CTL, WFN,     KC_RALT, KC_RGUI, KC_F14
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LPRN, KC_UP,   KC_RPRN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LEFT, KC_DOWN, KC_RGHT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_HOME, KC_PGDN, KC_LGUI, KC_F13,  QUO_ALT, GRV_CTL, SPC_SFT, WFN,     SPC_SFT, MIN_CTL, EQL_ALT, KC_F14,  KC_RGUI, KC_PGUP, KC_END
     ),
     [_WFN] = LAYOUT_ortho_5x15(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,  KC_PSCR, KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
@@ -77,9 +79,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MBASE] = LAYOUT_ortho_5x15(
         ADJUST,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LCBR, KC_EQL,  KC_RCBR, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_BSLS, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LPRN, KC_QUOT, KC_RPRN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_HOME, KC_UP,   KC_END,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_HAEN, KC_LCTL, KC_LALT, MFN,     GRV_GUI, SPC_SFT, KC_LEFT, KC_DOWN, KC_RGHT, SPC_SFT, MIN_GUI, MFN,     KC_RALT, KC_RCTL, KC_HANJ
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LPRN, KC_UP,   KC_RPRN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LEFT, KC_DOWN, KC_RGHT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_HOME, KC_PGDN, KC_LCTL, KC_HAEN, QUO_ALT, GRV_GUI, SPC_SFT, MFN,     SPC_SFT, MIN_GUI, EQL_ALT, KC_HANJ, KC_RCTL, KC_PGUP, KC_END
     ),
     [_MFN] = LAYOUT_ortho_5x15(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,  KC_PSCR, KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
